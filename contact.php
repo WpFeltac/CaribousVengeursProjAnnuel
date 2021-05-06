acb0df1ec993b6f03397c50d33a5ca15b350fca7
WpFeltac <67151803+WpFeltac@users.noreply.github.com>
2021-05-06 13:32:14 +0200

    Remise à neuf CSS, début mosaïque

--- contact.php
+++ contact.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Contact - Les Caribous Vengeurs</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-		<body>
 
-		<h1><center>Page de Contact</center></h1>
+	<head>
+		<title>Contact - Les Caribous Vengeurs</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap">
 
+	</head>
+
+	<body>
+
+		<div class="header">
+			<h1>Les Caribous Vengeurs - Page de Contact</h1>
+		</div>
+				
 		<br><br><br><br>
 <!--
 		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php"><b>Accueil</b></a></button>
@@ -20,7 +26,8 @@
 
 		<button type = "button" id="bouton"><a href="contact.php"><b>Contact</b></a></button>
 
-		<button type = "button" id="bouton"><a href="admin.php"><b>Administration</b></a></button>
+		<button type = "button" id="bouton"><a href="admin/admin.php"><b>Administration</b></a></button>
+
 		<br>
 
 	</body>
--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
+
 <html>
-<head>
-	<title>Les Caribous Vengeurs</title>
-	<link rel="stylesheet" href="style.css">
-</head>
+
+	<head>
+
+		<title>Les Caribous Vengeurs</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap">
+
+	</head>
+
 	<body>
-		
-<h1>
-	<center>Les Caribous Vengeurs</center>
-</h1>
+
+	<div class="header">		
+		<h1>
+			<center>Les Caribous Vengeurs</center>
+		</h1>
+	</div>
+
 	<br><br><br><br>
+
 <!--
 		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/index.php"><b>Accueil</b></a></button>
 
@@ -17,70 +27,74 @@
 
 		<button type = "button" id="bouton"><a href="https://www.je-code.com/esgi1/hberleur/annuaire/admin/admin.php"><b>Administration</b></a></button>
 -->
+
 		<button type = "button" id="bouton"><a href="index.php"><b>Accueil</b></a></button>
 
 		<button type = "button" id="bouton"><a href="contact.php"><b>Contact</b></a></button>
 
-		<button type = "button" id="bouton"><a href="admin.php"><b>Administration</b></a></button>
-	<br><br><br>			
-			<center>
-			<div id="liste">
-				<b>
-				
-				<a href="https://je-code.com/esgi1/adavid28/">Achille DAV.
-				</a>
+		<button type = "button" id="bouton"><a href="admin/admin.php"><b>Administration</b></a></button>
 
-			<br><br>
-			
-			
-				<a href="https://je-code.com/esgi1/atruffier/CV/">Aurelien TRU.
-				</a>
-			<br><br>
-			
-			
-				<a href="https://www.je-code.com/esgi1/avekemans/">Anaël VEK.</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/bkhemissi1/cv/">Bahya KHE.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/eelkihel/">Elias ELK.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/hberleur/cvHtml/">Hugo BER.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/hdoom/site/">Hugo DOO.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/jderache/">Julien DER.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/mboureau2/page/">Matthys BOU.
-				</a>
-			
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/qgransart/cours/">Quentin GRA.
-				</a>
-		
-			<br><br>
-			
-				<a href="https://je-code.com/esgi1/tdusautoir/cvphp/">Thibaut DUS.
-				</a>
-			</b>
+		<br><br><br>
+
+		<div class="centerist">
+
+			<?php 
+
+				$db = new PDO('mysql:host=exmachinefmci.mysql.db;dbname=exmachinefmci;charset=utf8', 'exmachinefmci', 'carp310M');
+			?>
+
+	    	<div class="container">
+	  			<h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	    			<h3>lol</h3>
+	  			</div>
 			</div>
-		</center>
+
+	
+			<div class="container">
+	  			<h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	    			<h3>lol</h3>
+	 			</div>
+			</div>
+	
+			<div class="container">
+	 			 <h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	   				<h3>lol</h3>
+	  			</div>
+			</div>
+
+			<div class="container">
+	  			<h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	    			<h3>lol</h3>
+	  			</div>
+			</div>
+
+			<div class="container">
+	 			 <h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	   				<h3>lol</h3>
+	  			</div>
+			</div>
+
+			<div class="container">
+	  			<h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	    			<h3>lol</h3>
+	  			</div>
+			</div>
+
+			<div class="container">
+	  			<h2>Prenom Nom</h2>
+	  			<div class="containerContent">
+	    			<h3>lol</h3>
+	  			</div>
+			</div>
+		</div>		
+
+
 	</body>
+
 </html>
